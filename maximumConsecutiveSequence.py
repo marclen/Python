874d05c4a7feb9567d1d490f7a40304b4b90d9bd
Marclen Chin <72699452+marclen@users.noreply.github.com>
2020-10-18 21:44:13 -0400

    Update maximumConsecutiveSequence.py

--- maximumConsecutiveSequence.py
+++ maximumConsecutiveSequence.py
@@ -1,49 +1,67 @@
+# Determining the maximum number of sequential numbers in a list
+# Using a loop in a loop, as we need to iterate through a list while keeping track of all the sequential numbers
+# Outer loop keeps track of where we currently are in the loop and instantiates sequence details
+# Inner loop keeps track of sequence details and records them
+# I wanted to record the details of each sequence, so I created an object and recorded them in a list
+
 class Sequence:
-    def __init__(self, minval, maxval, length):
-        self.minVal = minval
-        self.maxVal = maxval
+    def __init__(self, minimum_value, maximum_value, length):
+        self.minimum_value = minimum_value
+        self.maximum_value = maximum_value
         self.length = length
 
 
-providedList = [5, 2, 99, 3, 4, 1, 100, 9, 10, 11, 12, 13, 14, 15]
-providedList.sort()
+provided_list = [5, 2, 99, 3, 4, 1, 100, 11, 9, 14, 12, 10, 13, 15]
 
-sequencelist = []
+# code works whether your sort it or not of course
+provided_list.sort()
 
-outerLoopCount = 0
-sequenceBool = False
-while outerLoopCount < len(providedList):
-    currentMinSeq = providedList[outerLoopCount]
-    currentMaxSeq = providedList[outerLoopCount]
-    currentSequenceCount = 0
+sequence_list = []
 
-    # code to determine if the next is sequential, and then to iterate until  it no longer is
-    if outerLoopCount == len(providedList) - 1:
+loop_count = 0
+
+while loop_count < len(provided_list):
+    current_minimum_value = provided_list[loop_count]
+    current_maximum_value = provided_list[loop_count]
+    sequence_bool = False
+    current_sequence_length = 0
+
+    if loop_count == len(provided_list) - 1:
         # you're at the last value, don't think anything needs to be done here
         break
-    elif providedList[outerLoopCount + 1] - providedList[outerLoopCount] == 1:
-        sequenceBool = True
-        while sequenceBool and outerLoopCount < len(providedList):
 
-            currentSequenceCount += 1
+    # code to determine if the next item in list is a sequential number, and iterates until  it no longer is
+    elif provided_list[loop_count + 1] - provided_list[loop_count] == 1:
+        sequence_bool = True
 
-            if currentMinSeq > providedList[outerLoopCount]:
-                currentMinSeq = providedList[outerLoopCount]
+        # Keeps iterating until the next number is not sequential OR you're at the end of the list
+        while sequence_bool and loop_count < len(provided_list):
 
-            if currentMaxSeq < providedList[outerLoopCount]:
-                currentMaxSeq = providedList[outerLoopCount]
+            current_sequence_length += 1
 
-            if outerLoopCount == len(providedList) - 1:
+            if current_minimum_value > provided_list[loop_count]:
+                current_minimum_value = provided_list[loop_count]
+
+            if current_maximum_value < provided_list[loop_count]:
+                current_maximum_value = provided_list[loop_count]
+
+            if loop_count == len(provided_list) - 1:
+                # We want to be in here when we're at the last digit to record sequence information
+                # but we need to break before the next if statement, because it will throw out of range exception
                 break
-            if providedList[outerLoopCount + 1] - providedList[outerLoopCount] != 1:
-                sequenceBool = False
 
-            outerLoopCount += 1
-        sequencelist.append(Sequence(currentMinSeq, currentMaxSeq, currentSequenceCount))
+            if provided_list[loop_count + 1] - provided_list[loop_count] != 1:
+                sequence_bool = False
+
+            loop_count += 1
+        
+        # recording sequence details
+        sequence_list.append(Sequence(current_minimum_value, current_maximum_value, current_sequence_length))
+    
     else:
-        outerLoopCount += 1
+        loop_count += 1
 
-sequencelist.sort(key=lambda x: x.length, reverse=True)
+sequence_list.sort(key=lambda x: x.length, reverse=True)
 
-i = sequencelist[0]
-print(f'Sequence: Run: {i.length}, Min: {i.minVal}, Max: {i.maxVal}')
+for i in sequence_list:
+    print(f'Sequence: Run: {i.length}, Min: {i.minimum_value}, Max: {i.maximum_value}')
